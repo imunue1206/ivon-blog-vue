798e6c4869a277f8602b418020e6b6247aefa87d
imunu1206 <2777266702@qq.com>
2024-10-25 00:29:36 +0800

    使用vuetify以及新的交互设计语言，重构现有项目

--- imu-blog-view/src/main.js
+++ imu-blog-view/src/main.js
@@ -2,14 +2,25 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from './router'
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/reset.css';
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+//mdi
+import '@mdi/font/css/materialdesignicons.css'
 
 const app = createApp(App)
 const pinia = createPinia()
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 app.use(router)
-app.use(Antd)
+app.use(vuetify)
 app.use(pinia)
 
 app.mount('#app')
--- imu-blog-view/src/router/index.js
+++ imu-blog-view/src/router/index.js
@@ -1,32 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '@/views/LoginView.vue'
-import RegisterView from '@/views/RegisterView.vue'
 import IndexView from '@/views/IndexView.vue'
-import BlogRead from '@/components/BlogRead.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/login',
-      name: 'login',
-      component : LoginView
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView
-    },
-    {
       path: '/',
       name: 'index',
       component: IndexView
     },
-    {
-      path: '/read/:id',
-      name: "read",
-      component: BlogRead
-    }
+    
+    //more
   ]
 })
 
